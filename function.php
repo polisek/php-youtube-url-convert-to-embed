0ec87f138f66a109f667f126a61dcc1830613943
polisek <107623238+polisek@users.noreply.github.com>
2023-06-16 11:13:43 +0200

    Update function.php

--- function.php
+++ function.php
@@ -1,11 +1,16 @@
 function convertToEmbedURL($url) {
     $urlParts = parse_url($url);
-    parse_str($urlParts['query'], $queryParts);
-    if(isset($queryParts['v'])) {
-        return 'https://www.youtube.com/embed/' . $queryParts['v'];
-    } else {
-        return $url;
+    if($urlParts['host'] == 'youtu.be') {
+        // pro kratší URL youtu.be
+        return 'https://www.youtube.com/embed' . $urlParts['path'];
+    } else if($urlParts['host'] == 'www.youtube.com' || $urlParts['host'] == 'youtube.com') {
+        // pro běžné URL www.youtube.com
+        parse_str($urlParts['query'], $queryParts);
+        if(isset($queryParts['v'])) {
+            return 'https://www.youtube.com/embed/' . $queryParts['v'];
+        }
     }
+    return $url;
 }
 
 echo convertToEmbedURL("Enter URL")
